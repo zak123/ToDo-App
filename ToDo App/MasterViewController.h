e66bef954c09bb523d9297e7cc7f5a542ec6af38
zak123 <zacharym123@gmail.com>
2015-03-25 13:01:53 -0700

    Initial Commit

---
+++ ToDo App/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  ToDo App
+//
+//  Created by Zachary Mallicoat on 3/25/15.
+//  Copyright (c) 2015 cghcapital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
---
+++ ToDo App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDo App
+//
+//  Created by Zachary Mallicoat on 3/25/15.
+//  Copyright (c) 2015 cghcapital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
